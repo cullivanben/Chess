2e7fe9dff354f69a4baea3e48a97b3bd7c963b89
Benjamin Cullivan <bencullivan@Benjamins-MacBook-Pro-4.local>
2020-07-29 13:02:53 -0400

    fixed redis connection mistake

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 const guestIdRoute = '/set_guestid_46e5a98a-37a1-42d8-bb24-18be79ee95b0f99bf926-2b0a-4a82-a-da1833803723';
 
 // connect to redis
-var client = redis.createClient();
+var client = redis.createClient(process.env.REDIS_URL);
 
 // connect to the db
 mongoose.connect(
