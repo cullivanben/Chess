4b0cf68f97f86ba825cf7d8e385f2c5aca710c05
Benjamin Cullivan <bencullivan@Benjamins-MacBook-Pro-4.local>
2020-07-28 22:46:47 -0400

    redis

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const http = require('http');
 const express = require('express');
 const session = require('express-session');
 const socketIO = require('socket.io');
+const redis = require('redis');
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 const uuid = require('uuid');
 const path = require('path');
 require('dotenv').config();
 
-// the port to listen on
-const PORT = process.env.PORT || 5000;
+// connect to redis
+var client = redis.createClient();
 
 // connect to the db
 mongoose.connect(
@@ -31,24 +32,14 @@ var sessionMiddleWare = session({
 // set up express
 const app = express();
 app.use(sessionMiddleWare);
-
-// serve the react client
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// // DEVELOPMENT
-// app.get('/test', (req, res) => {
-//     // make sure the user has a guest id 
-//     console.log('guest from route ', req.session.guest);
-//     if (req.session.guest === undefined) {
-//         req.session.guest = uuid.v1();
-//     }
-//     res.send({ msg: 'yay' });
-// });
-
+// serve the robots.txt file
 app.get('/robots.txt', (req, res) => {
     res.sendFile(path.join(__dirname, 'robots.txt'));
 });
 
+// serve the react app
 app.get('*', (req, res) => {
     if (req.session.guest === undefined) {
         req.session.guest = uuid.v1();
@@ -63,8 +54,6 @@ const io = socketIO(server);
 // socket.io 
 var roomOpen = false;
 var currentId = uuid.v1();
-// maps each guest id to their room and color
-var guestInfo = new Map();
 
 // access session store within incoming socket.io connections
 io.use((socket, next) => sessionMiddleWare(socket.request, socket.request.res || {}, next));
@@ -75,82 +64,111 @@ io.on('connection', socket => {
     // get the guest id
     let guest = socket.request.session.guest;
 
-    // if this user is already part of a game, add them to the same room
-    if (guestInfo.has(guest)) socket.join(guestInfo.get(guest).room);
+    client.exists(guest, (err, reply) => {
+        // if this user is already part of a game, add them to the same room
+        if (reply === 1) {
+            console.log('joining same room');
 
-    // if the user is not currently part of a game
-    else {
-        // add them to the open game
-        if (!roomOpen) currentId = uuid.v1();
-        roomOpen = !roomOpen;
-        let roomId = currentId;
-        socket.join(roomId);
+            // get the room id from redis and join the room
+            client.get(guest, (err, reply) => socket.join(reply));
+        }
 
-        // determine the color of this user
-        let color = roomOpen ? 'white' : 'black';
+        // if the user is not currently part of a game, add them to the open game
+        // and save the roomId to redis with their guestId
+        else {
+            // add them to the open game
+            if (!roomOpen) currentId = uuid.v1();
+            roomOpen = !roomOpen;
+            let roomId = currentId;
+            socket.join(roomId);
 
-        // save the user's room and color
-        guestInfo.set(guest, { room: roomId, color: color });
+            // determine the color of this user
+            let color = roomOpen ? 'white' : 'black';
 
-        // send the color to the user
-        socket.emit('color', guestInfo.get(guest).color);
+            // save this user's room to redis
+            client.set(guest, roomId);
 
-        // if this is the second player to enter the room, tell both players that an opponent has connected
-        if (!roomOpen) io.in(roomId).emit('enemy-connected');
-    }
+            // send the color to the user
+            socket.emit('color', color);
+
+            // if this is the second player to enter the room, tell both players that an opponent has connected
+            if (!roomOpen) io.in(roomId).emit('enemy-connected');
+        }
+    });
 
     // when the board is updated, send the board update to the other user connected to this room
     socket.on('outgoing-board-update', data => {
-        socket.to(guestInfo.get(guest).room).emit('incoming-board-update', data);
+        client.get(guest, (err, reply) => {
+            socket.to(reply).emit('incoming-board-update', data);
+        });
     });
 
     // when a message is recieved, send the message to the other user
     socket.on('outgoing-message', data => {
-        socket.to(guestInfo.get(guest).room).emit('incoming-message', data);
+        client.get(guest, (err, reply) => {
+            socket.to(reply).emit('incoming-message', data);
+        });
     });
 
     // when the user emits their name, tell the other user
     socket.on('outgoing-name', data => {
-        socket.to(guestInfo.get(guest).room).emit('incoming-enemy-name', data);
+        client.get(guest, (err, reply) => {
+            socket.to(reply).emit('incoming-enemy-name', data);
+        });
     });
 
     // when the user is requesting a draw with the other user
     socket.on('outgoing-draw-request', () => {
-        socket.to(guestInfo.get(guest).room).emit('incoming-draw-request');
+        client.get(guest, (err, reply) => {
+            socket.to(reply).emit('incoming-draw-request');
+        });
     });
 
     // when a user accepts another user's draw request
     socket.on('its-a-draw', () => {
-        socket.to(guestInfo.get(guest).room).emit('match-was-draw');
-        // remove this player from the map of guest info
-        if (guestInfo.has(guest)) guestInfo.delete(guest);
+        // tell the other user that the match was a draw
+        client.get(guest, (err, reply) => {
+            socket.to(reply).emit('match-was-draw');
+        });
+        // remove this player from redis
+        client.del(guest);
         // disconnect the socket
         socket.disconnect(true);
-    })
+    });
 
-    // when a user loses, tell the other user that they have won
+    // when a user loses
     socket.on('i-lost', () => {
-        socket.to(guestInfo.get(guest).room).emit('you-won');
-        // remove this player from the map of guest info
-        if (guestInfo.has(guest)) guestInfo.delete(guest);
+        // tell the other user that they have won
+        client.get(guest, (err, reply) => {
+            socket.to(reply).emit('you-won');
+        });
+        // remove this player from redis
+        client.del(guest);
         // disconnect the socket
         socket.disconnect(true);
     });
 
     // forcibly disconnect the user
     socket.on('force-disconnect', () => {
-        // inform the other player that this player left the room
-        socket.to(guestInfo.get(guest).room).emit('enemy-left');
-        // remove this player from the map of guest info
-        if (guestInfo.has(guest)) guestInfo.delete(guest);
-        // disconnect the socket
-        socket.disconnect(true);
+        // if this user is still in redis
+        client.exists(guest, (err, reply) => {
+            if (reply === 1) {
+                // tell the other user that this user left
+                client.get(guest, (err, reply) => {
+                    socket.to(reply).emit('enemy-left');
+                    socket.disconnect(true);
+                });
+                // remove this player from redis
+                client.del(guest);
+            } 
+            else socket.disconnect(true);
+        });
     });
 
     // handles when this user is the second user to disconnect
     socket.on('secondary-disconnect', () => {
-        // remove this player from the map of guest info
-        if (guestInfo.has(guest)) guestInfo.delete(guest);
+        // remove this player from redis
+        client.del(guest);
         // disconnect the socket
         socket.disconnect(true);
     });
@@ -161,9 +179,112 @@ io.on('connection', socket => {
     });
 });
 
-if (process.env.NODE_ENV === 'production') {
-    
-}
-
 // start listening on the specified port
-server.listen(PORT);
+server.listen(process.env.PORT || 5000);
+
+
+
+    // // if this user is already part of a game, add them to the same room
+    // if (guestInfo.has(guest)) socket.join(guestInfo.get(guest).room);
+
+
+    // if the user is not currently part of a game
+    // else {
+    //     // add them to the open game
+    //     if (!roomOpen) currentId = uuid.v1();
+    //     roomOpen = !roomOpen;
+    //     let roomId = currentId;
+    //     socket.join(roomId);
+
+    //     // determine the color of this user
+    //     let color = roomOpen ? 'white' : 'black';
+
+    //     // // save the user's room and color
+    //     // guestInfo.set(guest, { room: roomId, color: color });
+
+    //     // save this user's room
+
+    //     // send the color to the user
+    //     socket.emit('color', guestInfo.get(guest).color);
+
+    //     // if this is the second player to enter the room, tell both players that an opponent has connected
+    //     if (!roomOpen) io.in(roomId).emit('enemy-connected');
+    // }
+
+
+
+
+       // // when the board is updated, send the board update to the other user connected to this room
+    // socket.on('outgoing-board-update', data => {
+    //     socket.to(guestInfo.get(guest).room).emit('incoming-board-update', data);
+    // });
+
+    // // when a message is recieved, send the message to the other user
+    // socket.on('outgoing-message', data => {
+    //     socket.to(guestInfo.get(guest).room).emit('incoming-message', data);
+    // });
+
+    // // when the user emits their name, tell the other user
+    // socket.on('outgoing-name', data => {
+    //     socket.to(guestInfo.get(guest).room).emit('incoming-enemy-name', data);
+    // });
+
+    // // when the user is requesting a draw with the other user
+    // socket.on('outgoing-draw-request', () => {
+    //     socket.to(guestInfo.get(guest).room).emit('incoming-draw-request');
+    // });
+
+    // // when a user accepts another user's draw request
+    // socket.on('its-a-draw', () => {
+    //     socket.to(guestInfo.get(guest).room).emit('match-was-draw');
+    //     // remove this player from the map of guest info
+    //     if (guestInfo.has(guest)) guestInfo.delete(guest);
+    //     // disconnect the socket
+    //     socket.disconnect(true);
+    // });
+
+    // // when a user loses, tell the other user that they have won
+    // socket.on('i-lost', () => {
+    //     socket.to(guestInfo.get(guest).room).emit('you-won');
+    //     // remove this player from the map of guest info
+    //     if (guestInfo.has(guest)) guestInfo.delete(guest);
+    //     // disconnect the socket
+    //     socket.disconnect(true);
+    // });
+
+    // // forcibly disconnect the user
+    // socket.on('force-disconnect', () => {
+    //     // inform the other player that this player left the room
+    //     socket.to(guestInfo.get(guest).room).emit('enemy-left');
+    //     // remove this player from the map of guest info
+    //     if (guestInfo.has(guest)) guestInfo.delete(guest);
+    //     // disconnect the socket
+    //     socket.disconnect(true);
+    // });
+
+    // // handles when this user is the second user to disconnect
+    // socket.on('secondary-disconnect', () => {
+    //     // remove this player from the map of guest info
+    //     if (guestInfo.has(guest)) guestInfo.delete(guest);
+    //     // disconnect the socket
+    //     socket.disconnect(true);
+    // });
+
+    // // forcibly disconnect the message socket
+    // socket.on('disconnect-message', () => {
+    //     socket.disconnect(true);
+    // });
+
+
+    // // maps each guest id to their room and color
+// var guestInfo = new Map();
+
+// // DEVELOPMENT
+// app.get('/test', (req, res) => {
+//     // make sure the user has a guest id 
+//     console.log('guest from route ', req.session.guest);
+//     if (req.session.guest === undefined) {
+//         req.session.guest = uuid.v1();
+//     }
+//     res.send({ msg: 'yay' });
+// });
